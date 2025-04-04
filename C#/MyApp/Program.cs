ff15820830e2f96391a35409d0b250866345c76e
keith-school <k.e.steeby@gmail.com>
2025-04-04 08:53:39 -0500

    Add files via upload

---
+++ C#/MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter a word:");
+string? inputWord = Console.ReadLine();
+inputWord = inputWord ?? string.Empty;
+Dictionary<char, int> letters = new Dictionary<char, int>();
+
+for (int i = 0; i < inputWord.Length; i++)
+{
+    char upperChar = char.ToUpper(inputWord[i]);
+    if (char.IsLetter(upperChar))
+    {
+        if (letters.ContainsKey(upperChar))
+            letters[upperChar]++;
+        else
+            letters[upperChar] = 1;
+    }
+}
+
+Console.WriteLine("Character counts:");
+foreach (var i in letters.Keys.OrderBy(a => a))
+    Console.WriteLine(i + ": " + letters[i]);
