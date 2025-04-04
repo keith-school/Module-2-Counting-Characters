92e6c7c04f42fbeadabaf8763813e711f0e884f5
keith-school <k.e.steeby@gmail.com>
2025-04-04 09:47:49 -0500

    Add files via upload

---
+++ C# Rectangle/MyApp/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Rectangle length:");
+double recLength = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Rectangle width:");
+double recWidth = Convert.ToDouble(Console.ReadLine());
+
+double recArea = recLength * recWidth;
+Console.WriteLine("The rectangle's area is: " + recArea);
